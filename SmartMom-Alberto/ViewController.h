04612e539b7d5af5739bec2e19d8ad27763f3135
Alberto Lagos <alberto.lagos@gmail.com>
2015-03-09 13:01:03 -0300

    Initial Commit

---
+++ SmartMom-Alberto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartMom-Alberto
+//
+//  Created by Alberto Lagos on 09-03-15.
+//  Copyright (c) 2015 Alberto Lagos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
