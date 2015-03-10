29012bb489661826ab66deeebd9acb6164e996bd
Alberto Lagos <alberto.lagos@gmail.com>
2015-03-10 18:52:17 -0300

    Changes in SMAContactTest
    This due a new way of getting SMAContact objects.

--- SmartMom-Alberto/Tests/SMAContactTest.m
+++ SmartMom-Alberto/Tests/SMAContactTest.m
@@ -25,8 +25,8 @@
     
     self.contact.firstName = firstName;
     self.contact.lastName  = lastName;
-    self.contact.phoneNumbers = @[@"(773) 570-6321"];
-    self.contact.emails       = @[@"john@doe.com"];
+    self.contact.phoneNumber = @"(773) 570-6321";
+    self.contact.email       = @"john@doe.com";
 
 }
 
@@ -42,67 +42,28 @@
     XCTAssertTrue([self.contact.fullName isEqualToString:fullName]);
 }
 
-- (void)testThatItContactWithPhoneNumberAndMail
-{
-    XCTAssertEqual(self.contact.type, SMAContactTypeBoth);
-}
-
 - (void)testThatItContactWithOutContactInfo
 {
-    self.contact.emails         = nil;
-    self.contact.phoneNumbers   = nil;
+    self.contact.email          = nil;
+    self.contact.phoneNumber   = nil;
     XCTAssertEqual(self.contact.type, SMAContactTypeNone);
 }
 
 - (void)testThatItContactWithOnlyPhoneNumber
 {
-    self.contact.emails       = nil;
-    self.contact.phoneNumbers = @[@"(773) 570-6321"];
+    self.contact.email       = nil;
+    self.contact.phoneNumber = @"(773) 570-6321";
     
     XCTAssertEqual(self.contact.type, SMAContactTypePhone);
 }
 
 - (void)testThatItContactWithOnlyMail
 {
-    self.contact.phoneNumbers = nil;
-    self.contact.emails       = @[@"john@doe.com"];
+    self.contact.phoneNumber = nil;
+    self.contact.email       = @"john@doe.com";
     
     XCTAssertEqual(self.contact.type, SMAContactTypeMail);
 }
-
-
-
-//- (void)testThatItSucceedGrantAccessAddressBook {
-//    SMAAddressBookManager *abManager = [[SMAAddressBookManager alloc] init];
-//    
-//    //Expectation
-//    XCTestExpectation *expectation = [self expectationWithDescription:@"Ask for access to Address Book"];
-//    
-//    [abManager askForAuthorizationWithCompletionBlock:^(BOOL success, NSArray *contacts, NSError *error) {
-//        
-//    }];
-//
-////        if(error)
-////        {
-////            NSLog(@"error is: %@", error);
-////        }else{
-////            NSInteger statusCode = [httpResponse statusCode];
-////            XCTAssertEqual(statusCode, 200);
-////            [expectation fulfill];
-////        }
-////        
-////    }];
-//    
-//    
-//    [self waitForExpectationsWithTimeout:5.0 handler:^(NSError *error) {
-//        
-//        if(error)
-//        {
-//            XCTFail(@"Expectation Failed with error: %@", error);
-//        }
-//        
-//    }];
-//}
 
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
