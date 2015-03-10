e163544460bd261153fbccde6dc0c2651035462d
Alberto Lagos <alberto.lagos@gmail.com>
2015-03-10 18:50:37 -0300

    Common button.
    This is a simple button that appear in the cells.

--- SmartMom-Alberto/Buttons/SMAButton.h
+++ SmartMom-Alberto/Buttons/SMAButton.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "UIImage+ColorFromImage.h"
 
 @interface SMAButton : UIButton
 
--- SmartMom-Alberto/Buttons/SMAButton.m
+++ SmartMom-Alberto/Buttons/SMAButton.m
@@ -7,15 +7,36 @@
 //
 
 #import "SMAButton.h"
+#define SELECTED_COLOR [UIImage imageFromColor:[UIColor colorWithRed:199/255.0f green:79/255.0f blue:114/255.0f alpha:1.0]]
+#define NORMAL_COLOR   [UIImage imageFromColor:[UIColor colorWithRed:238/255.0f green:215/255.0f blue:174/255.0f alpha:1.0]]
 
 @implementation SMAButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if(self) {
+        [self setBackgroundImage:NORMAL_COLOR forState:UIControlStateNormal];
+        [self setBackgroundImage:SELECTED_COLOR forState:UIControlStateSelected];
+        [self setBackgroundImage:SELECTED_COLOR forState:UIControlStateHighlighted];
+        self.layer.masksToBounds = YES;
+        self.layer.cornerRadius = 5.0f;
+    }
+    
+    return self;
 }
-*/
 
+- (instancetype)initWithFrame:(CGRect)aFrame
+{
+    self = [super initWithFrame:aFrame];
+    if(self) {
+        [self setBackgroundImage:NORMAL_COLOR forState:UIControlStateNormal];
+        [self setBackgroundImage:SELECTED_COLOR forState:UIControlStateSelected];
+        [self setBackgroundImage:SELECTED_COLOR forState:UIControlStateHighlighted];
+        self.layer.masksToBounds = YES;
+        self.layer.cornerRadius = 5.0f;
+    }
+    
+    return self;
+}
 @end
